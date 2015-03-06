e3a3416395c2ded58a182bf9b8f0874769b164de
wzz <757140740@qq.com>
2015-03-06 17:49:31 +0800

    emoji表情文字与编码的替换

--- js/action/chatAction.js
+++ js/action/chatAction.js
@@ -81,11 +81,10 @@ $(document).click(function(){
 	$('.smiliesAll').fadeOut("300");
 })
 //点击表情
+var str;
 $('.hotSmilies li').on('click',function(){
 	$(".inputArea textarea").focus();	//获取输入框焦点
-
 	$(".inputArea textarea").val($(".inputArea textarea").val() + '[' + $(this).attr("title") + ']');
-
 	//$(".inputArea textarea").val($(".inputArea textarea").val() + $(this).find('.emoji').attr("alt"));
 	//$('.textareaDiv').html('<pre>'+$(this).find('.emoji').attr("alt")+'</pre>');
 	twemoji.parse(document.getElementsByTagName('body')[0], {size: 36});
@@ -109,6 +108,24 @@ var myNotificationListener = {
 		var newChatMessage = chatMessage['text'].replace(/\n/g, '_@').replace(/\r/g, '_#');
 			newChatMessage = newChatMessage.replace(/_#_@/g, '<br/>');//IE7-8
 			newChatMessage = newChatMessage.replace(/_@/g, '<br/>');//IE9、FF、chrome
+			newChatMessage = newChatMessage.replace(/\[哭脸\]/g, '&#x1F602;');
+			newChatMessage = newChatMessage.replace(/\[笑脸\]/g, '&#x1F601;');
+			newChatMessage = newChatMessage.replace(/\[哭脸\]/g, '&#x1F602;');
+			newChatMessage = newChatMessage.replace(/\[脸1\]/g, '&#x1F603;');
+			newChatMessage = newChatMessage.replace(/\[脸2\]/g, '&#x1F604;');
+			newChatMessage = newChatMessage.replace(/\[脸3\]/g, '&#x1F605;');
+			newChatMessage = newChatMessage.replace(/\[脸4\]/g, '&#x1F606;');
+			newChatMessage = newChatMessage.replace(/\[脸5\]/g, '&#x1F607;');
+			newChatMessage = newChatMessage.replace(/\[脸6\]/g, '&#x1F608;');
+			newChatMessage = newChatMessage.replace(/\[脸7\]/g, '&#x1F609;');
+			newChatMessage = newChatMessage.replace(/\[脸8\]/g, '&#x1F60A;');
+			newChatMessage = newChatMessage.replace(/\[脸9\]/g, '&#x1F60B;');
+			newChatMessage = newChatMessage.replace(/\[脸10\]/g, '&#x1F60C;');
+			newChatMessage = newChatMessage.replace(/\[脸11\]/g, '&#x1F60D;');
+			newChatMessage = newChatMessage.replace(/\[脸12\]/g, '&#x1F60E;');
+			newChatMessage = newChatMessage.replace(/\[脸13\]/g, '&#x1F60F;');
+			newChatMessage = newChatMessage.replace(/\[脸14\]/g, '&#x1F610;');
+			newChatMessage = newChatMessage.replace(/\[脸15\]/g, '&#x1F611;');
 		if($.trim(chatMessage['text']) == ''){
 		}else{
 			var MeMessageReceived = '';
